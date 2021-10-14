3c6011d91a36ab5c079c8d81f0a1cf71909a68ec
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-10-14 09:53:28 -0300

    Projeto Criado

---
+++ src/main/java/br/com/ayrton/excecoesspringapirest/ExcecoesspringapirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.ayrton.excecoesspringapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcecoesspringapirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcecoesspringapirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ayrton/excecoesspringapirest/ExcecoesspringapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ayrton.excecoesspringapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcecoesspringapirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
